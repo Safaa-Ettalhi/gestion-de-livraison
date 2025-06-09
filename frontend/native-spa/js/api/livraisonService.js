203f6e82f5f31f9832143e1782fa641e95a20af0
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-06-09 19:02:22 +0100

    Ajout des fonctions pour gérer les livraisons (récupération, ajout, modification, suppression, consultation par ID), ainsi qu’une fonction pour récupérer la liste des colis

---
+++ frontend/native-spa/js/api/livraisonService.js
@@ -0,0 +1,48 @@
+const baseUrl = 'http://localhost:8000';
+
+export const getLivraisons = async () => {
+    const response = await fetch(`${baseUrl}/api/v1/livraisons`);
+    return response.json();
+};
+
+export const getColis = async () => {
+    const response = await fetch(`${baseUrl}/api/v1/colis`);
+    return response.json();
+};
+
+export const getLivraison = async (id) => {
+    const response = await fetch(`${baseUrl}/api/v1/livraisons/${id}`);
+    if (!response.ok) {
+        throw new Error(`Error fetching livraison with id ${id}: ${response.statusText}`);
+    }
+    return response.json();
+};  
+
+
+
+export const deleteLivraison = async (id) => {
+    const response = await fetch(`${baseUrl}/api/v1/livraisons/${id}`, {
+        method: 'DELETE',
+    });
+    return response.ok;
+};
+
+
+export const editLivraison = async (id, data) => {
+    const response = await fetch(`${baseUrl}/api/v1/livraisons/${id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    });
+    return response.ok;
+};
+
+export const addLivraison = async (formData) => {
+    const response = await fetch(`${baseUrl}/api/v1/livraisons`, {
+        method: 'POST',
+        body: formData,
+    });
+    return response.ok;
+};
+
+export const getFile = (path) => `${baseUrl}/files/v1?path=${path}`;
